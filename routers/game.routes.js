72efa46530f89bb6234de6b13e4ab32517e665e8
slabak <slabakxaker@gmail.com>
2022-07-16 18:06:05 +0300

    без камелкейсов class

--- routers/game.routes.js
+++ routers/game.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 //  Миделвееры;
 const authCurent = require("./middleware/auth/auth.middleware");
-const gameStart = require("../controllers/gamestart");
+const gameStart = require("../controllers/gamestart/GameStart");
 
 router.get("/", authCurent, gameStart.gamersRender);
 
