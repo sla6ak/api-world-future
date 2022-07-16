e2c1057b2d21d8a5b3c94d94ff47d5a7689931d0
slabak <slabakxaker@gmail.com>
2022-07-16 10:42:26 +0300

    cors options

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const { BASE_URL } = process.env;
 const PORT = process.env.PORT || 5000; // http://localhost:5000/docs
 
 // Список настроек для сервера
-app.use(cors({ origin: "*" }));
+app.options("*", cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/auth", routerAuth);
